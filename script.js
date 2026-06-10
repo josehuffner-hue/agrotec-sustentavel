18d8c1de6f14be2997194009ed831dbac85500e8
josehuffner-hue <jose.huffner@escola.pr.gov.br>
2026-06-10 08:14:18 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Projeto Agrinho 2026 carregado com sucesso!");
+
+    const ano = new Date().getFullYear();
+    document.getElementById("ano").textContent = ano;
+});
